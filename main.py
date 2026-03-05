4b32c6f98f5255512bd64ae2cf06d1bf035e9371
allyeodream <azet_kr@naver.com>
2026-03-05 18:44:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ def get_orders(token, days=1):
         "Content-Type": "application/json",
         "X-Cafe24-Api-Version": "2025-12-01"
     }
-    params = {"start_date": start, "end_date": end, "limit": 100}
+    params = {"start_date": start, "end_date": end, "limit": 100, "canceled": "F"}
     r = requests.get(url, headers=headers, params=params)
     return r.json().get("orders", [])
 
